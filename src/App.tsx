fd2685a8c94925eb7ccee79c66ff54746c2644a5
SvichV <svych.vlad@gmail.com>
2023-07-07 02:45:25 +0300

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
 import React from 'react';
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom';
 import './App.css';
 import { HeroSelection } from "./components/hero-selection/hero-selection";
 
 export const App = () => {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Navigate to='/hero-selection'/>
+    },
+    {
+      path: '/hero-selection',
+      element:  <HeroSelection />
+    },
+    {
+      path: '/pre-fight',
+      element: <div>PreFight</div>
+    }
+  ])
+
 
   return (
-    <HeroSelection />
+    <RouterProvider router={router}/>
   );
 }
